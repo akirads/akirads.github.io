036ede2d5d162a8e8031cb6ab830894a52a1e9fa
NeverLore <52979674+NeverLore@users.noreply.github.com>
2019-10-11 23:40:02 +0300

    READY

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>A.KIRA</title>
+	<link rel="stylesheet" type="text/css" href="mybttn.css">
+	<style>
+		table {
+			width: 100%; border-spacing: 0; 
+		}
+		body {
+			background-image: url(back.png)
+		}
+	</style>
+</head>
+<body>
+	<h1 style="color: #fff; position: relative;text-align: center;">A.KIRA</h1>
+	<br><br><br>
+	<div class="block_3" style="position: relative; left: 15%;">
+		<div class="block_2" style="position: relative; left: 0.5%; right: 0.5%; top: 2%; bottom: 2%;">
+			<div class="block_1" style="position: relative; left: 0.5%;top: 2%; bottom: 2%; right: 0.5%;">
+				<div class="text_1" style="position: relative;top: 50px;">
+					Oh, hi!
+				</div>
+				<div style="position: relative; top: 40px;">
+					<div class="text_1">
+						You are on AKIRAs page, here you can join to 
+					</div>
+					<div class="text_1">
+						us <a>Discord</a> channel! Common!
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<button class="button_1" onclick="location.href='https://discord.gg/hDPpYqH'" style="position: relative;top: 170px;left: 85%;right: 5%;">
+		<strong>JOIN</strong>
+	</button>
+</body>
+</html>
